e5fc975f024c62865d83c03845cf404de180049f
csd-hienmai <work.hienmai@gmail.com>
2024-07-22 12:29:00 +0700

    feat: get computer chocie

---
+++ script.js
@@ -0,0 +1,16 @@
+const computerSelection = getcomputerChoice();
+
+function getcomputerChoice(){
+    let choice = Math.floor((Math.random() * 2))
+    if (choice === 0) {
+        return "Rock"
+    }
+    else if (choice === 1) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
+}
+
+console.log(computerSelection)
