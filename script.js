6fa8cb5d24e710eeb0503dc26acd177a8292c44a
csd-hienmai <work.hienmai@gmail.com>
2024-07-22 13:16:35 +0700

    feat: play a single round

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const computerSelection = getcomputerChoice();
-const humanSelection = getHumanChoice();
 let humanScore = 0;
 let computerScore = 0;
 
@@ -7,26 +5,73 @@ let computerScore = 0;
 function getcomputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   if (choice === 0) {
-    return "Rock";
+    return "rock";
   } else if (choice === 1) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
 function getHumanChoice() {
-  let choice = prompt("Enter a chocie (rock/paper/scissors): ");
+  let choice = prompt("Enter a chocie (rock/paper/scissors): ").toLowerCase();
   if (choice === "rock") {
-    return "Rock";
+    return "rock";
   } else if (choice === "paper") {
-    return "Paper";
-  } else if (choice === "Scissors") {
-    return "Scissors";
+    return "paper";
+  } else if (choice === "scissors") {
+    return "scissors";
   } else {
-    return "Invalid choice";
+    return "invalid choice";
   }
 }
 
+const computerSelection = getcomputerChoice();
+const humanSelection= getHumanChoice();
+
+function playGround(humanChoice, computerChoice) {
+    let message = "It's a tie!"
+    if (humanChoice === computerChoice) {
+        humanScore +=1
+        computerScore +=1
+    }
+    else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore +=1
+            message = "Paper covers rock! You lose."
+        }
+        else if (computerChoice === "scissors") {
+            humanScore +=1
+            message = "Rock smashes scissors. You win."
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore +=1
+            message = "Paper covers rock! You win."
+        }
+        else if (computerChoice === "scissors") {
+            computerScore +=1
+            message = "Scissors cuts paper! You lose."
+        }
+    }
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore +=1
+            message = "Rock smashes scissors! You lose."
+        }
+        else if (computerChoice === "paper") {
+            humanScore +=1
+            message = "Scissors cuts paper! You win."
+        }
+    }
+    return message
+
+}
+
+
+
 console.log("Computer chocie: " + computerSelection);
-console.log("Your chocie: " + humanSelection);
+console.log("Your choice: " + humanSelection);
+console.log(playGround(humanSelection, computerSelection))
+console.log("Your score: " + humanScore, "Computer score: " + computerScore);
